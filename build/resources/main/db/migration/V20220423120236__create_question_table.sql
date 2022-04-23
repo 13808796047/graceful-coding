f7995c8ea781845174721afc44e5c9f746fbc61e
summer <511391805@qq.com>
2022-04-23 15:25:15 +0800

    :feat 环境文件抽离

--- build/resources/main/db/migration/V20220423120236__create_question_table.sql
+++
@@ -1,8 +0,0 @@
-create table question
-(
-    id int not null auto_increment,
-    questioner_id varchar(30) not null,
-    title  varchar(30) not null,
-    detail  text null,
-    primary key (id)
-)
